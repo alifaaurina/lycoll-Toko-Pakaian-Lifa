683167cfd539ff40b3fdc5649de19e49392021fc
alifaaurina <alifaaurina@gmail.com>
2026-03-11 15:19:38 +0700

    update lagi

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,19 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // IMPORT ROUTES
 const authRoutes = require('./src/routes/auth');
 const userRoutes = require('./src/routes/users');
